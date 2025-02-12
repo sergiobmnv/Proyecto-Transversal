3a20849de910ce5aceaeab8fa53ab1303502923e
sergiobmnv <sergiobmnv@gmail.com>
2025-02-12 19:06:06 +0100

    Creacion del proyecto Spring

---
+++ ProyectoTransversal/CRFLOSKYCASINO/src/main/java/com/CRFLOSKYCASINO/CrfloskycasinoApplication.java
@@ -0,0 +1,13 @@
+package com.CRFLOSKYCASINO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrfloskycasinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrfloskycasinoApplication.class, args);
+	}
+
+}
