9b7ec453a7755a231b647142572ca27da015f621
crmigueel <miguelcuadradocr@gmail.com>
2025-02-19 09:09:31 +0100

    Cambio a RestController

--- ProyectoTransversal/CRFLOSKYCASINO/src/main/java/com/CRFLOSKYCASINO/Controller/UsuarioController.java
+++ ProyectoTransversal/CRFLOSKYCASINO/src/main/java/com/CRFLOSKYCASINO/Controller/UsuarioController.java
@@ -1,16 +1,16 @@
 package com.CRFLOSKYCASINO.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.CRFLOSKYCASINO.Model.UsuarioDTO;
 import com.CRFLOSKYCASINO.Service.UsuarioService;
 
-@Controller
+@RestController
 @RequestMapping("/usuario")
 public class UsuarioController {
     
