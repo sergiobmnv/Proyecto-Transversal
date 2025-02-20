5bb7ce9d197362a1278161a7b2af8d3c8fbba1ed
crmigueel <miguelcuadradocr@gmail.com>
2025-02-20 10:43:52 +0100

    cambio isBlank por si el nombre es nulo

--- ProyectoTransversal/CRFLOSKYCASINO/src/main/java/com/CRFLOSKYCASINO/Controller/UsuarioController.java
+++ ProyectoTransversal/CRFLOSKYCASINO/src/main/java/com/CRFLOSKYCASINO/Controller/UsuarioController.java
@@ -44,7 +44,7 @@ public class UsuarioController {
 			respuesta = "404";
 		}else {
 			 usuarioDTO = usuarioService.validarUsuario(usuario, pwd);
-			 if(usuarioDTO.getNombre().isBlank()) {
+			 if(usuarioDTO.getNombre()==null) {
 				 respuesta = "Contraseña incorrecta";
 			 }else {
                 //Si el Login ha sido correcto, guardamos al usuario en la sesión.
